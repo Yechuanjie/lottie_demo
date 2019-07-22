cf3ead46ff2e3447aed7f0ec9dc8bf59d5e8e57f
yechuanjie <1102684711@qq.com>
2019-07-22 16:37:05 +0800

    快应用跳转测试

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -4,12 +4,19 @@ import './list.scss';
 
 const List: FC<Lotties> = (list) => {
   const lotties = list.lotties
+  const toQuickApp = () => {
+    window.location.href = 'hap://app/com.calendar.dym';
+  }
   return (
-    <ul>
-      {lotties.map(item => 
-        <li key={item.id}>{item.name}</li>
-      )}
-    </ul>
+    <div>
+      <ul>
+        {lotties.map(item =>
+          <li key={item.id}>{item.name}</li>
+        )}
+      </ul>
+      <a className="link" href="hap://app/com.calendar.dym">To Quick App</a>
+      <div className="link" onClick={toQuickApp}>To Quick App</div>
+    </div>
   );
 }
 export default List;
