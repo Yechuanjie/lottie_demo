ee071002e730960a9080cb40219b4d277c037b77
hwq <1102684711@qq.com>
2020-07-10 17:42:16 +0800

    更新样式

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import './index.scss';
-import lottieList from '../../assets//lottieFiles/lottieInfo';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
+import lottieList from "../../assets//lottieFiles/lottieInfo";
 
-import PageHead from '../../components/pagehead/pagehead';
-import List from '../../components/list/list';
+import PageHead from "../../components/pagehead/pagehead";
+import List from "../../components/list/list";
 
 const Index: React.FC = () => {
   return (
     <div className="index_page">
       <PageHead></PageHead>
       <List lotties={lottieList}></List>
+
+      <div
+        className="tip"
+        onClick={() => {
+          window.open("https://lottiefiles.com/featured");
+        }}
+      >
+        获取最新Lottie动画
+      </div>
     </div>
   );
-}
+};
 
 export default Index;
