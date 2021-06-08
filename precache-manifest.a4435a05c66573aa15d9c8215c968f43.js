6edaeed740117015d2ebb1cd815b79ab0a4de209
yechuanjie <1102684711@qq.com>
2021-06-08 02:39:37 +0000

    Update

---
+++ precache-manifest.a4435a05c66573aa15d9c8215c968f43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122d6072377e2afcc322a182a0a5b089",
+    "url": "/lottie_demo/index.html"
+  },
+  {
+    "revision": "22a1069ac602c87817f2",
+    "url": "/lottie_demo/static/css/main.03f0c62a.chunk.css"
+  },
+  {
+    "revision": "f4426ad3d4aa55046cc7",
+    "url": "/lottie_demo/static/js/2.7f31488c.chunk.js"
+  },
+  {
+    "revision": "22a1069ac602c87817f2",
+    "url": "/lottie_demo/static/js/main.caa232ba.chunk.js"
+  },
+  {
+    "revision": "69367e2f3cd9f50f6bb3",
+    "url": "/lottie_demo/static/js/runtime~main.3dd116ad.js"
+  },
+  {
+    "revision": "9c92c7878160fb712ba0f18120427c2a",
+    "url": "/lottie_demo/static/media/fast-forward.9c92c787.svg"
+  },
+  {
+    "revision": "7ceaad2b8217b2604ad0d42d2789a7fc",
+    "url": "/lottie_demo/static/media/pause.7ceaad2b.svg"
+  },
+  {
+    "revision": "46c2be8f869a27fc5e5bd6985883a9cc",
+    "url": "/lottie_demo/static/media/start.46c2be8f.svg"
+  }
+]);
